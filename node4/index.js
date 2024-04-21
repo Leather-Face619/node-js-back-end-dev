5d41dbf268fdd37be946037c45cefc224021cb45
Leather-Face619 <rahul852369hhhhh@gmail.com>
2024-04-21 10:21:31 +0530

    2nd

--- node4/index.js
+++ node4/index.js
@@ -6,13 +6,18 @@ const app = express();
 app.set('view engine', 'ejs');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'index')))
+app.use(express.static(path.join(__dirname, "/public")))
 
 app.get('/', (req, res) => {
     fs.readdir('./files', (err, files) => {
         res.render('index', { files: files })
     })
-})  
+})
+app.get('/file/:fname', (req, res) => {
+    fs.readFile(`files/${req.params.fname}.txt`, 'utf8', (err,fdata) => {
+       res.render('show',{filename: req.params.fname,filedata: fdata})
+    })
+})
 app.post('/create', (req, res) => {
     fs.writeFile(`./files/${req.body.title.split(" ").join("_")}.txt`, `${req.body.details}`, (err) => {
         console.log(err)
